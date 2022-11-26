267137651f1612de5bfde5f88b241c4889792a83
nata-non <63070045@kmitl.ac.th>
2022-11-26 22:30:19 +0700

    feat : create room

--- screens/CreateRoomScreen.js
+++ screens/CreateRoomScreen.js
@@ -1,8 +1,165 @@
-import { NativeBaseProvider } from 'native-base/lib/typescript/core/NativeBaseContext'
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import InterestBadge from "../components/InterestBadge";
+import {
+  NativeBaseProvider,
+  ScrollView,
+  Text,
+  Input,
+  Button,
+} from "native-base";
+import { View, StyleSheet } from "react-native";
+import { TextInput } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import { useFonts, Ribeye_400Regular } from "@expo-google-fonts/ribeye";
+import { Ionicons } from "@expo/vector-icons";
+import { color } from "react-native-elements/dist/helpers";
+import { TouchableOpacity } from "react-native";
 
 export default function CreateRoomScreen() {
+  const [fontsLoaded] = useFonts({
+    Ribeye_400Regular,
+  });
+  const [search, setSearch] = React.useState("");
+  const [select, setSelect] = React.useState([]);
+
+  function selectHandle(name) {
+    let clone = [...select];
+
+    if (!clone.includes(name)) {
+      clone.push(name);
+    } else {
+      clone.splice(clone.indexOf(name), 1);
+    }
+
+    setSelect(clone);
+  }
+  useEffect(() => {
+    console.log(select);
+  }, [select]);
+
+  useEffect(() => {
+    console.log(search);
+    console.log(
+      interestName.filter((name) => name.includes(search.toLowerCase()))
+    );
+  }, [search]);
+
+  const interestName = [
+    "basketball",
+    "football",
+    "ai",
+    "pressing",
+    "newcomer",
+    "detective",
+    "invest",
+    "noice",
+  ];
   return (
-    <NativeBaseProvider></NativeBaseProvider>
-  )
+    <NativeBaseProvider>
+      <ScrollView style={[{ marginTop: "10%" }]}>
+        <View style={styles.textIcon}>
+          <View style={{ paddingHorizontal: 20 }}>
+            <Text
+              fontSize="3xl"
+              style={[{ textAlign: "start", fontFamily: "Ribeye_400Regular" }]}
+            >
+              Room Name
+            </Text>
+            {/* <Input style={{ borderRadius: 20 }}></Input> */}
+            <TextInput
+              placeholder="BOBO RETURN"
+              style={{
+                height: 40,
+                width: "100%",
+                margin: 12,
+                borderWidth: 1,
+                padding: 10,
+                borderRadius: 10,
+                alignSelf: "center",
+                borderColor: "#9D746B",
+              }}
+            />
+            <Text
+              fontSize="3xl"
+              style={[{ textAlign: "start", fontFamily: "Ribeye_400Regular" }]}
+            >
+              interest topic
+            </Text>
+            {/* <Input onChangeText={setSearch} placeholder={"Search"}></Input> */}
+            <TextInput
+              placeholder="Search"
+              onChangeText={setSearch}
+              style={{
+                height: 40,
+                width: "100%",
+                margin: 12,
+                borderWidth: 1,
+                padding: 10,
+                borderRadius: 10,
+                alignSelf: "center",
+                borderColor: "#9D746B",
+              }}
+            />
+            {
+              <View style={styles.BtnSelection}>
+                {select.map((name, index) => (
+                  <InterestBadge
+                    key={name}
+                    idx={index}
+                    text={name}
+                    onToggled={() => selectHandle(name)}
+                    dataLists={select}
+                  />
+                ))}
+              </View>
+            }
+            <TouchableOpacity
+              style={{
+                marginTop: "5%",
+                backgroundColor: "#9D746B",
+                padding: 8,
+                borderRadius: 10,
+              }}
+            >
+              <Text style={{ textAlign: "center" }}>CreateRoom</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        {
+          <View style={styles.BtnSelection}>
+            {interestName
+              .filter((name) => name.includes(search.toLowerCase()))
+              .map((name, index) => {
+                if (!select.includes(name))
+                  return (
+                    <InterestBadge
+                      key={name}
+                      idx={index}
+                      text={name}
+                      onToggled={() => selectHandle(name)}
+                      dataLists={select}
+                    />
+                  );
+              })}
+          </View>
+        }
+      </ScrollView>
+    </NativeBaseProvider>
+  );
 }
+const styles = StyleSheet.create({
+  textIcon: {
+    textAlign: "center",
+    marginTop: "15%",
+  },
+  BtnSelection: {
+    flexDirection: "row",
+    justifyContent: "center",
+    flexWrap: "wrap",
+    marginTop: "10%",
+    // maxHeight:"50%"
+  },
+  BtnStyle: {
+    borderColor: "white",
+  },
+});
--- screens/InterestSelectScreen.js
+++ screens/InterestSelectScreen.js
@@ -1,46 +1,123 @@
-import React from 'react'
-import InterestBadge from '../components/InterestBadge'
-import { NativeBaseProvider, ScrollView, Text, Input } from 'native-base'
-import { View, StyleSheet } from 'react-native'
-import { AntDesign } from '@expo/vector-icons';
-import { useFonts, Ribeye_400Regular } from '@expo-google-fonts/ribeye';
-import { Ionicons } from '@expo/vector-icons';
-
+import React from "react";
+import InterestBadge from "../components/InterestBadge";
+import { NativeBaseProvider, ScrollView, Text, Input } from "native-base";
+import { View, StyleSheet, TextInput } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import { useFonts, Ribeye_400Regular } from "@expo-google-fonts/ribeye";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function InterestSelectScreen({ navigation }) {
   const [fontsLoaded] = useFonts({
     Ribeye_400Regular,
   });
-  const interestName = ["basketball", "football", "ai", "pressing", "newcomer", "detective", "invest"]
+  const [search, setSearch] = React.useState("");
+  const [select, setSelect] = React.useState([]);
+  function selectHandle(name) {
+    let clone = [...select];
+
+    if (!clone.includes(name)) {
+      clone.push(name);
+    } else {
+      clone.splice(clone.indexOf(name), 1);
+    }
+
+    setSelect(clone);
+  }
+  const interestName = [
+    "basketball",
+    "football",
+    "ai",
+    "pressing",
+    "newcomer",
+    "detective",
+    "invest",
+  ];
   return (
     <NativeBaseProvider>
       <ScrollView style={[{ marginTop: "10%" }]}>
         <View style={styles.textIcon}>
-          <Text fontSize="3xl" style={[{ textAlign: 'center', fontFamily: 'Ribeye_400Regular' }]}>What's your</Text>
-          <Text fontSize="3xl" style={[{ textAlign: 'center', fontFamily: 'Ribeye_400Regular' }]}>interest topic</Text>
+          <Text
+            fontSize="3xl"
+            style={[{ textAlign: "center", fontFamily: "Ribeye_400Regular" }]}
+          >
+            What's your
+          </Text>
+          <Text
+            fontSize="3xl"
+            style={[{ textAlign: "center", fontFamily: "Ribeye_400Regular" }]}
+          >
+            interest topic
+          </Text>
         </View>
-        <View style={styles.BtnSelection}>
-          {interestName.map((name, index) => {
-            return <InterestBadge key={index} idx={index} text={name} />
-          })}
-        </View>
-        <View style={[{ flex: 1, alignItems: 'flex-end' }]}>
-          <AntDesign name="arrowright" size={24} color="black" onPress={() => { navigation.navigate("RoomQuery") }} />
+        <TextInput
+          placeholder="Search"
+          onChangeText={setSearch}
+          style={{
+            height: 40,
+            width: "80%",
+            margin: 12,
+            borderWidth: 1,
+            padding: 10,
+            borderRadius: 10,
+            alignSelf: "center",
+            borderColor: "#9D746B",
+          }}
+        />
+        {
+          <View style={styles.BtnSelection}>
+            {select.map((name, index) => (
+              <InterestBadge
+                key={name}
+                idx={index}
+                text={name}
+                onToggled={() => selectHandle(name)}
+                dataLists={select}
+              />
+            ))}
+          </View>
+        }
+        {
+          <View style={styles.BtnSelection}>
+            {interestName
+              .filter((name) => name.includes(search.toLowerCase()))
+              .map((name, index) => {
+                if (!select.includes(name))
+                  return (
+                    <InterestBadge
+                      key={name}
+                      idx={index}
+                      text={name}
+                      onToggled={() => selectHandle(name)}
+                      dataLists={select}
+                    />
+                  );
+              })}
+          </View>
+        }
+        <View style={[{ flex: 1, alignItems: "flex-end" }]}>
+          <AntDesign
+            name="arrowright"
+            size={24}
+            color="black"
+            onPress={() => {
+              navigation.navigate("RoomQuery");
+            }}
+          />
         </View>
       </ScrollView>
     </NativeBaseProvider>
-  )
+  );
 }
 const styles = StyleSheet.create({
   textIcon: {
-    textAlign: 'center',
+    textAlign: "center",
     marginTop: "15%",
   },
   BtnSelection: {
     flexDirection: "row",
-    justifyContent: 'center',
-    flexWrap: 'wrap',
+    justifyContent: "center",
+    flexWrap: "wrap",
     marginTop: "10%",
     // maxHeight:"50%"
-  }
+  },
 });
